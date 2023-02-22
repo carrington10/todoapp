f2e3a24f02ed82aa0bc73dc1877ab8be8a078703
cooper <carringtonhate@gmail.com>
2023-02-22 11:45:39 -0800

    adding in dev branch

--- src/main/java/com/in28minutes/springboot/webapp/login/LoginController.java
+++ src/main/java/com/in28minutes/springboot/webapp/login/LoginController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory; // logging
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -12,11 +13,15 @@ public class LoginController {
     //http
      private Logger log = LoggerFactory.getLogger(getClass());
     // model put it into model
-    @RequestMapping("login")
-     public String gotoLoginPage(@RequestParam String name, ModelMap model){
-        model.put("name",name);  // model map class
-        log.debug("request params is {}", name); // logging will never be printed to prod logs
-        log.info(" I want to print this "); // loging
+    @RequestMapping(value="login",method = RequestMethod.GET)
+     public String gotoLoginPage(){
         return "login";
+     }// end of login page 
+
+     @RequestMapping(value="login",method = RequestMethod.POST)
+     public String gotoWelcomepage(@RequestParam String name, @RequestParam String password, ModelMap map){
+        map.put("name", name);
+        map.put("password",password);
+        return "welcome";
      }// end of login page 
 }// end of login controller 
---
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
@@ -0,0 +1,9 @@
+<html>
+	<head>
+		<title> Welcome</title>
+	</head>
+	<body>
+		 <div> Welcome to todo list </div>
+		 <div> Your name: ${name}</div>
+	</body>
+</html>
